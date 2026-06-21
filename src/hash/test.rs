909f0a9f47a480edcaea71037352b5417d417215
HypnosCicero <hypnoscicero200@gmail.com>
2026-06-21 23:24:35 +0800

    feat: use borrow model

--- src/hash/test.rs
+++ src/hash/test.rs
@@ -5,13 +5,13 @@ mod hash_algorithm_tests {
     #[test]
     fn test_decode2binary_vector() {
         let binary_vector = hash::algorithm::decode2binary_vector("123".as_bytes());
-        assert!(juede_u8_vector_is_binary(binary_vector));
+        assert!(juede_u8_vector_is_binary(&binary_vector));
     }
 
-    fn juede_u8_vector_is_binary(vector: Vec<u8>) -> bool {
+    fn juede_u8_vector_is_binary(vector: &Vec<u8>) -> bool {
         let mut result = true;
         for elemnt in vector {
-            if elemnt > 1 {
+            if *elemnt > 1 {
                 result = false;
             }
         }
