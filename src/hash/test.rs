da971bf90e85becf1288a4ec613278431e6430ca
HypnosCicero <hypnoscicero200@gmail.com>
2026-06-21 21:25:46 +0800

    feat: add the test decode2binary vectory function

--- src/hash/test.rs
+++ src/hash/test.rs
@@ -1,2 +1,20 @@
 #[cfg(test)]
-mod hash_algorithm_tests {}
+mod hash_algorithm_tests {
+    use crate::hash;
+
+    #[test]
+    fn test_decode2binary_vector() {
+        let binary_vector = hash::algorithm::decode2binary_vector("123".as_bytes());
+        assert!(juede_u8_vector_is_binary(binary_vector));
+    }
+
+    fn juede_u8_vector_is_binary(vector: Vec<u8>) -> bool {
+        let mut result = true;
+        for elemnt in vector {
+            if elemnt > 1 {
+                result = false;
+            }
+        }
+        result
+    }
+}
